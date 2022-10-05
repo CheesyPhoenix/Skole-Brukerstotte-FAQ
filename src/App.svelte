eedd0a6f981a470a63e66775f347afda0bb9b4ab
cheesyphoenix <sindre.he.moe@gmail.com>
2022-10-05 09:38:16 +0200

    tags workie

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,33 @@
 
 	let questionArr = questions.slice();
 
+	let tags: string[] = [];
+	questionArr.forEach((question) => {
+		const re = RegExp(/[^.] ([A-ZÆØÅ][^ \.?",\n:]{2,})/gm);
+
+		let m: RegExpExecArray;
+
+		let q = question.question.slice();
+		do {
+			m = re.exec(q);
+			if (m) {
+				if (!tags.includes(m[1])) tags.push(m[1]);
+			}
+		} while (m);
+
+		q = question.answer.slice();
+		do {
+			m = re.exec(q);
+			if (m) {
+				if (!tags.includes(m[1])) tags.push(m[1]);
+			}
+		} while (m);
+	});
+
+	tags.sort((a, b) => {
+		return a.localeCompare(b);
+	});
+
 	let searchTerm: string;
 
 	$: {
@@ -12,16 +39,57 @@
 
 			for (let i = 0; i < questions.length; i++) {
 				const question = questions[i];
-				if (
-					question.question
-						.toLowerCase()
-						.includes(searchTerm.toLowerCase())
-				) {
+
+				let includes = true;
+				for (let x = 0; x < searchTerm.split(" ").length; x++) {
+					const term = searchTerm.split(" ")[x];
+
+					if (
+						!(question.question + ". " + question.answer)
+							.toLowerCase()
+							.includes(term.toLowerCase())
+					) {
+						includes = false;
+					}
+				}
+				if (includes) {
 					newQuestions.push(question);
 				}
 			}
 
 			questionArr = newQuestions;
+		}
+	}
+
+	let selectedTags: HTMLElement[] = [];
+
+	function tagClick(
+		ev: MouseEvent & {
+			currentTarget: EventTarget & HTMLHeadingElement;
+		},
+		tag: string
+	) {
+		if (!searchTerm) searchTerm = "";
+
+		if (selectedTags.includes(ev.target as HTMLElement)) {
+			searchTerm = searchTerm.replace(tag, "").trim();
+		} else {
+			searchTerm += " " + tag;
+			searchTerm = searchTerm.trim();
+		}
+
+		if ((ev.target as HTMLElement).className.includes("selected")) {
+			(ev.target as HTMLElement).className = (
+				ev.target as HTMLElement
+			).className.replace(" selected", "");
+
+			selectedTags.splice(
+				selectedTags.indexOf(ev.target as HTMLElement),
+				1
+			);
+		} else {
+			(ev.target as HTMLElement).className += " selected";
+			selectedTags.push(ev.target as HTMLElement);
 		}
 	}
 </script>
@@ -31,7 +99,30 @@
 
 	<br />
 
-	<input id="searchBox" bind:value={searchTerm} type="text" />
+	<input
+		id="searchBox"
+		bind:value={searchTerm}
+		on:keypress={() => {
+			selectedTags.forEach(
+				(el) => (el.className = el.className.replace(" selected", ""))
+			);
+
+			selectedTags = [];
+		}}
+		type="text"
+	/>
+
+	<div id="tags">
+		{#each tags as tag}
+			<h5
+				on:click={(ev) => {
+					tagClick(ev, tag);
+				}}
+			>
+				#{tag}
+			</h5>
+		{/each}
+	</div>
 
 	<hr />
 
@@ -57,5 +148,25 @@
 	hr {
 		margin-top: 1em;
 		margin-bottom: 1em;
+	}
+	#tags {
+		display: flex;
+		justify-content: center;
+		flex-direction: row;
+		flex-wrap: wrap;
+	}
+
+	#tags > h5 {
+		border: solid 2px #f5f5f5;
+		width: max-content;
+		padding: 0.5em;
+		transition-duration: 0.3s;
+		margin: 0 0.5em;
+		margin-top: 1em;
+	}
+	#tags > h5:hover {
+		background-color: #ccc;
+		color: #333;
+		cursor: pointer;
 	}
 </style>
